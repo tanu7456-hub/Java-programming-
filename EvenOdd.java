9a239692c8d7336087e6095033095d18bb7cfd3c
Tanu <tanutanvi7456@gmail.com>
2026-04-04 11:01:57 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+// to find even odd number without using arithmetic operator 
+class EvenOdd
+{
+     static int a =5;
+    public static void main(String[] args){
+        if((a&1)==0){ 
+            
+            System.out.println(a+" is even number.");
+        }
+        else{
+            System.out.println(a+ " is odd number");
+        }
+    }
+    
+}
